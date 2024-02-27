032773aac4371075e5bbca6250c0b3a9882e0352
judoree <135936771+judoree@users.noreply.github.com>
2024-02-27 17:17:21 +0900

    header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Box, Button, HStack } from "@chakra-ui/react";
+import { Link, Outlet } from "react-router-dom";
+import { FaAirbnb } from "react-icons/fa";
 
 export default function Root() {
   return (
-    <h1>
-      im root
+    <Box>
+      <HStack justifyContent={"space-between"} py={5} px={10} borderBottomWidth={1}>
+        <Box color="red.500">
+          <Link to={"/"}>
+            <FaAirbnb size={"48"} />
+          </Link>
+        </Box>
+        <HStack spacing={2}>
+          <Button>Log in</Button>
+          <Button colorScheme={"red"}>Sign up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-      {/* Outlet이 하는 일은 루트의 중앙에 내가 렌더링 하고싶은 컴포넌트 위치 */}
-    </h1>
+    </Box>
   );
 }
