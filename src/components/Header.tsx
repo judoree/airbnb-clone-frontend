b936171573f17acd9ac423598d7fcb2c6e1c6f21
judoree <135936771+judoree@users.noreply.github.com>
2024-02-28 17:20:49 +0900

    Dark

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,31 @@
-import { FaAirbnb, FaMoon } from "react-icons/fa";
-import { Box, Button, HStack, IconButton, useDisclosure } from "@chakra-ui/react";
+import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
+import { Box, Button, HStack, IconButton, LightMode, useColorMode, useColorModeValue, useDisclosure } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
+import { color } from "framer-motion";
 
 export default function Header() {
   const { isOpen: isLoginOpen, onClose: onLoginClose, onOpen: onLoginOpen } = useDisclosure();
   const { isOpen: isSignUpOpen, onClose: onSignUpClose, onOpen: onSignUpOpen } = useDisclosure();
+  const { toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue("red.500", "red.200");
+  const Icon = useColorModeValue(FaMoon, FaSun);
   return (
     <HStack justifyContent={"space-between"} py={5} px={10} borderBottomWidth={1}>
-      <Box color="red.500">
+      <Box color={logoColor}>
         <Link to={"/"}>
           <FaAirbnb size={"48"} />
         </Link>
       </Box>
       <HStack spacing={2}>
-        <IconButton variant={"ghost"} aria-label="Toggle dark mode" icon={<FaMoon />} />
+        <IconButton onClick={toggleColorMode} variant={"ghost"} aria-label="Toggle dark mode" icon={<Icon />} />
         <Button onClick={onLoginOpen}>Log in</Button>
-        <Button onClick={onSignUpOpen} colorScheme={"red"}>
-          Sign up
-        </Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme={"red"}>
+            Sign up
+          </Button>
+        </LightMode>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
       <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       {/* provider는 자동으로 자신의 모든 childeren내부에 모든 설정과 구성 사항들을 포함 */}
       <RouterProvider router={router} />
     </ChakraProvider>
