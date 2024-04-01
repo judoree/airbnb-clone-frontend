2d82e799d2f73123dbd000fe1674133c8a63b8b2
judoree <135936771+judoree@users.noreply.github.com>
2024-04-01 16:56:44 +0900

    Router Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
