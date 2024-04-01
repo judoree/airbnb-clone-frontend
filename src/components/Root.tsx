81c484f9d2684dc7b7b5fea926d90bd686d6a2c6
judoree <135936771+judoree@users.noreply.github.com>
2024-04-01 19:32:31 +0900

    Log In Modal

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,8 +1,26 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  Modal,
+  useDisclosure,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Input,
+  VStack,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
 import { Link, Outlet } from "react-router-dom";
-import { FaAirbnb } from "react-icons/fa";
+import { FaAirbnb, FaMoon, FaUserNinja, FaLock } from "react-icons/fa";
 
 export default function Root() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
     <Box>
       <HStack justifyContent={"space-between"} py={5} px={10} borderBottomWidth={1}>
@@ -12,9 +30,32 @@ export default function Root() {
           </Link>
         </Box>
         <HStack spacing={2}>
-          <Button>Log in</Button>
+          <IconButton variant={"ghost"} aria-label="Toggle dark mode" icon={<FaMoon />} />
+          <Button onClick={onOpen}>Log in</Button>
           <Button colorScheme={"red"}>Sign up</Button>
         </HStack>
+        <Modal motionPreset="slideInBottom" onClose={onClose} isOpen={isOpen}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Log in</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                <InputGroup>
+                  <InputLeftElement children={<FaUserNinja />} />
+                  <Input variant={"filled"} placeholder="Username" />
+                </InputGroup>
+                <InputGroup>
+                  <InputLeftElement children={<FaLock />} />
+                  <Input variant={"filled"} placeholder="password" />
+                </InputGroup>
+              </VStack>
+              <Button mt={4} colorScheme="red" w="100%">
+                Log in
+              </Button>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
       </HStack>
       <Outlet />
     </Box>
--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <VStack bg="black.100" justifyContent={"center"} minH="100vh">
+    <VStack bg="dark" justifyContent={"center"} minH="100vh">
       <Heading>Page not found.</Heading>
       <Text>It seems that you're lost.</Text>
       <Link to="/">
