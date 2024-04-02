de01c1b86012209c2d12f837e79d8ac7a570362d
judoree <135936771+judoree@users.noreply.github.com>
2024-04-02 17:18:44 +0900

    Manual Fetching

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,38 @@
 import { FaStar, FaRegHeart } from "react-icons/fa";
 import { Box, Grid, Heading, HStack, Image, Text, VStack, Button, Skeleton, SkeletonText } from "@chakra-ui/react";
 import Room from "../components/Room";
+import { useEffect, useState } from "react";
+import RoomSkeleton from "../components/RoomSkeleton";
+
+interface IPhoto {
+  pk: string;
+  file: string;
+  description: string;
+}
+
+interface IRoom {
+  pk: number;
+  name: string;
+  country: string;
+  city: string;
+  price: number;
+  rating: number;
+  is_owner: boolean;
+  photos: IPhoto[];
+}
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [rooms, setRooms] = useState<IRoom[]>([]);
+  const fetchRooms = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
+    const json = await response.json();
+    setRooms(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchRooms();
+  }, []);
   return (
     <Grid
       mt={10}
@@ -20,12 +50,23 @@ export default function Home() {
         "2xl": "repeat(5 , 1fr)",
       }}
     >
-      <Box>
-        <Skeleton height={280} rounded="2xl" mb={8} />
-        <SkeletonText w="50%" noOfLines={2} mb={7} />
-        <SkeletonText w="20%" noOfLines={1} />
-      </Box>
-      <Room />
+      {isLoading ? (
+        <>
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+        </>
+      ) : null}
+      {rooms.map((room) => (
+        <Room imageUrl={room.photos[0].file} name={room.name} rating={room.rating} city={room.city} country={room.country} price={room.price} />
+      ))}
     </Grid>
   );
 }
