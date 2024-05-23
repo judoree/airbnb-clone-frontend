e93da85679bb1c6e1883adb02331b10f25e45341
judoree <135936771+judoree@users.noreply.github.com>
2024-05-23 15:26:25 +0900

    Protected Pages

---
+++ src/routes/UploadRoom.tsx
@@ -0,0 +1,12 @@
+import useHostOnlyPage from "../components/HostOnlyPage";
+import HostOnlyPage from "../components/HostOnlyPage";
+import ProtectedPage from "../components/ProtectedPage";
+
+export default function UploadRoom() {
+  useHostOnlyPage();
+  return (
+    <ProtectedPage>
+      <h1>upload room!</h1>
+    </ProtectedPage>
+  );
+}
