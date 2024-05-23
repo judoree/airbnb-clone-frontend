6b7f7c6eabcd3d1e6d651be22da53761c647d5b4
judoree <135936771+judoree@users.noreply.github.com>
2024-05-23 17:09:05 +0900

    Dynamic Form

--- src/routes/UploadRoom.tsx
+++ src/routes/UploadRoom.tsx
@@ -2,23 +2,30 @@ import {
   Box,
   Container,
   FormControl,
+  Grid,
   Heading,
   VStack,
   FormLabel,
   FormHelperText,
   Input,
   InputGroup,
+  Button,
   InputLeftAddon,
   Textarea,
   Checkbox,
   Select,
 } from "@chakra-ui/react";
 import useHostOnlyPage from "../components/HostOnlyPage";
-import HostOnlyPage from "../components/HostOnlyPage";
 import ProtectedPage from "../components/ProtectedPage";
 import { FaBed, FaToilet } from "react-icons/fa";
+import { useQuery } from "@tanstack/react-query";
+import { getAmenities, getCategories } from "../api";
+import { IAmenity, ICategory } from "../types";
+// const { data: categories, isLoading: isCategoriesLoading } = useQuery<ICategory>(["categories"], getCategories);
 
 export default function UploadRoom() {
+  const { data: amenities, isLoading: isAmenitiesLoading } = useQuery<IAmenity[]>(["amenities"], getAmenities); // API 에서 amenitis 가져온 함수
+  const { data: categories, isLoading: isCategoriesLoading } = useQuery<ICategory[]>(["categories"], getCategories);
   useHostOnlyPage();
   return (
     <ProtectedPage>
@@ -32,7 +39,7 @@ export default function UploadRoom() {
       >
         <Container>
           <Heading textAlign={"center"}>Upload Room</Heading>
-          <VStack spacing={5} as="form" mt={5}>
+          <VStack spacing={10} as="form" mt={5}>
             <FormControl isRequired>
               <FormLabel>Room Name</FormLabel>
               <Input focusBorderColor="lime" required type="text" />
@@ -87,6 +94,31 @@ export default function UploadRoom() {
               </Select>
               <FormHelperText>What kind of room are you renting?</FormHelperText>
             </FormControl>
+            <FormControl>
+              <FormLabel>Category</FormLabel>
+              <Select placeholder="Choose a kind">
+                {categories?.map((category) => (
+                  <option key={category.pk} value={category.pk}>
+                    {category.name}
+                  </option>
+                ))}
+              </Select>
+              <FormHelperText>What category describes your room?</FormHelperText>
+            </FormControl>
+            <FormControl>
+              <FormLabel>Amenities</FormLabel>
+              <Grid templateColumns={"1fr 1fr"} gap={5}>
+                {amenities?.map((amenity) => (
+                  <Box key={amenity.pk}>
+                    <Checkbox>{amenity.name}</Checkbox>
+                    <FormHelperText>{amenity.description}</FormHelperText>
+                  </Box>
+                ))}
+              </Grid>
+            </FormControl>
+            <Button size="lg" height="48px" width="100%" border="2px" borderColor="green.500">
+              Upload Room
+            </Button>
           </VStack>
         </Container>
       </Box>
